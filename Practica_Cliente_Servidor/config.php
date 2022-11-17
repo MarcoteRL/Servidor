3e55fad0f1104c37e56263947fe385b71f6a48e1
Marcote <andres@Marcote.localdomain>
2022-11-17 22:19:47 +0100

    Update

--- Practica_Cliente_Servidor/config.php
+++ Practica_Cliente_Servidor/config.php
@@ -4,8 +4,15 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'practica');
 
+$create_db = 'CREATE DATABASE if not exists practica';
+if (mysql_query($connect, $create_db)) {
+    // echo "Database practica created successfully\n";
+} 
+
+
 $connect = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if ($connect === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
